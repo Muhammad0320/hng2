febe6bfc0fd6d3af1845515871235c439c9ae59c
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-06 09:29:07 +0100

    chore:  update crypto modeule

--- services/Crypto.ts
+++ services/Crypto.ts
@@ -2,6 +2,27 @@ import { promisify } from "util";
 
 import { randomBytes, scrypt } from "crypto";
 
-const scrypr = promisify(scrypt);
+const scryptAsync = promisify(scrypt);
 
-export class CryptoManager {}
+export class CryptoManager {
+  async hash(password: string): Promise<string> {
+    const salt = randomBytes(8).toString("hex");
+
+    const hashedPassword = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return `${hashedPassword.toString("hex")}.${salt}`;
+  }
+
+  async compare(
+    storedPassword: string,
+    suppliedPassword: string
+  ): Promise<boolean> {
+    const [hashedPassword, salt] = storedPassword.split(".");
+
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return `${buf.toString("hex")}` === hashedPassword;
+  }
+}
+
+
