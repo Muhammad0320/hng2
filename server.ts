051e22dbf5d9f9f81d2a69d6a5e06313463dc88a
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-06 08:24:10 +0100

    chore:  update server ts

--- server.ts
+++ server.ts
@@ -1,2 +1,13 @@
 
-// mongodb+srv://muhammad0320:muhammawwal@005@atlascluster.x1qxlkc.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster
+// mongodb+srv://muhammad0320:muhammawwal@005@atlascluster.x1qxlkc.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster 
+
+import mongoose from "mongoose";
+
+if( ! process.env.MONGO_URI) throw new Error(' Mongo uri not found ')
+if( ! process.env.PORT) throw new Error(' port uri not found ')
+
+    const port = process.env.PORT || 8000;
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log( 'DB connection successful' ));
+
+
