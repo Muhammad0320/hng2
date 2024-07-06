6fbbcc9c340b40ce0ee97f43699268a50bb74c31
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-06 09:11:00 +0100

    chore:  add organization virtual user

--- src/model/User.ts
+++ src/model/User.ts
@@ -19,35 +19,50 @@ type UserModel = mongoose.Model<UserDoc> & {
 };
 
 
-const userSchema = new mongoose.Schema<UserDoc, UserModel>({
-  firstName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
+const userSchema = new mongoose.Schema<UserDoc, UserModel>(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
 
-  lastName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
 
-  email: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: true,
-  },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
 
-  password: {
-    type: String,
-    select: false,
-    required: true,
-  },
+    password: {
+      type: String,
+      select: false,
+      required: true,
+    },
 
-  phone: {
-    type: Number,
-    required: true,
-    trim: true,
+    phone: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
   },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        (ret.userId = ret._id), delete ret._id;
+      },
+    },
+  }
+);
+
+userSchema.virtual("organisations", {
+  foreignField: "users",
+  localField: "_id",
+  ref: "Organisation",
 });
