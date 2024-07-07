dc91a7340db4876497e4b19ed3ec3ab7a82cf100
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-07 17:05:50 +0100

    chore:  update validator ts

--- src/services/validators.ts
+++ src/services/validators.ts
@@ -1,26 +1,30 @@
 import { check } from "express-validator";
 
-export const emailValidator = check("email")
-  .trim()
-  .isEmail()
-  .withMessage("Please provide a valid email format ");
+export const emailValidator = () =>
+  check("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email format ");
 
-export const passwordValidator = check("password")
-  .trim()
-  .notEmpty()
-  .isLength({ min: 8 })
-  .withMessage("Please provide a valid password format");
+export const passwordValidator = () =>
+  check("password")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 8 })
+    .withMessage("Please provide a valid password format");
 
-export const passwordConfirmValidator = check("password")
-  .trim()
-  .notEmpty()
-  .custom((input: string, { req }) => input === req.body.password)
-  .withMessage("Passwords does not match");
+export const passwordConfirmValidator = () =>
+  check("password")
+    .trim()
+    .notEmpty()
+    .custom((input: string, { req }) => input === req.body.password)
+    .withMessage("Passwords does not match");
 
-export const phoneValidator = check("phone")
-  .isInt()
-  .isMobilePhone("en-NG")
-  .withMessage("Please provide nigerian mobile phone no");
+export const phoneValidator = () =>
+  check("phone")
+    .isInt()
+    .isMobilePhone("en-NG")
+    .withMessage("Please provide nigerian mobile phone no");
 
 export const firstNameValidator = (field: string) =>
   check(field)
