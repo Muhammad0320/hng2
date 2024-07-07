1095b0391209a048ba687e4fcdb9a4f1c3125aa7
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-07 17:50:39 +0100

    chore: update all ts

--- src/routes/org/all.ts
+++ src/routes/org/all.ts
@@ -1,5 +1,19 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { requireAuth } from "../../middleware/requireAuth";
+import Org from "../../model/Organisation";
 
 const router = express.Router();
+
+router.get("/", requireAuth, async (req: Request, res: Response) => {
+  const alluserOrg = await Org.find({ userId: req.currentUser.userId });
+
+  res.status(200).json({
+    status: "success",
+    message: "Load data",
+    data: {
+      orgs: alluserOrg,
+    },
+  });
+});
 
 export { router as allOrgRouter };
