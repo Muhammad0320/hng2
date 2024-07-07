195f55eaf1f5bba29b5bed878bb6e83a97eb3585
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-07 17:40:43 +0100

    chore:  update show ts

--- src/routes/auth/show.ts
+++ src/routes/auth/show.ts
@@ -1,5 +1,28 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { requireAuth } from "../../middleware/requireAuth";
+import { paramsChecker } from "../../middleware/paramsChecker";
+import User from "../../model/User";
+import { BadRequest } from "../../error/BadRequest";
 
 const router = express.Router();
+
+router.get(
+  "/api/users/:id",
+  requireAuth,
+  paramsChecker("id"),
+  async (req: Request, res: Response) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) throw new BadRequest("User not found");
+
+    res.status(200).json({
+      status: "suceess",
+      message: "Load data",
+      data: {
+        user,
+      },
+    });
+  }
+);
 
 export { router as showRouter };
