8b3f99633417b3d682dd0ca042f7ec6d15ef2f39
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 04:49:18 +0100

    chore:  add 2nd test

--- src/__test__/auth/login.spec.ts
+++ src/__test__/auth/login.spec.ts
@@ -6,4 +6,21 @@ it("returns a 400 on invalid email ", async () => {
     .post("/api/auth/login")
     .send({ email: "shitmail", password: "shitpassword" })
     .expect(400);
+
+  await request(app)
+    .post("/api/auth/login")
+    .send({ password: "shitpassword" })
+    .expect(400);
+});
+
+it("returns a 400 on invalid password", async () => {
+  await request(app)
+    .post("/api/auth/login")
+    .send({ email: "shitmail@gmail.com", password: "shit" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/auth/login")
+    .send({ email: "shitmail@gmail.com" })
+    .expect(400);
 });
