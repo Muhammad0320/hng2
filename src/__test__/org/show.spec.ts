c46ddc49df4c1b19c0b1299967a03e9604210fa6
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 17:50:43 +0100

    chore:  update show spec ts

--- src/__test__/org/show.spec.ts
+++ src/__test__/org/show.spec.ts
@@ -7,7 +7,7 @@ import { orgBuilder, userBuilder } from "../builder";
 it("returns a 401 for unauthorized request", async () => {
   await request(app)
     .get(`/api/organisations/shitid`)
-
+    .send()
     .expect(401);
 });
 
@@ -15,6 +15,8 @@ it("returns a 400 for invlalid monogoose id", async () => {
   await request(app)
     .get(`/api/organisations/shitid`)
     .set("Cookie", await global.signin())
+    .send()
+
     .expect(400);
 });
 
@@ -24,6 +26,8 @@ it("returns a 404 for valid but umatched id", async () => {
   await request(app)
     .get(`/api/organisations/${id}`)
     .set("Cookie", await global.signin())
+    .send()
+
     .expect(404);
 });
 
@@ -34,6 +38,8 @@ it("returns a 400 if a user tried to access an org s/he does not have access", a
   await request(app)
     .get(`/api/organisations/${org.id}`)
     .set("Cookie", await global.signin())
+    .send()
+
     .expect(400);
 });
 
@@ -46,5 +52,7 @@ it("returns a 200 when wverything is valid", async () => {
   await request(app)
     .get(`/api/organisations/${org.id}`)
     .set("Cookie", await global.signin(user.id))
+    .send()
+
     .expect(200);
 });
