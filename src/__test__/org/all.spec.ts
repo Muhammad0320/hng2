7ebbf737dad03714036107c3690501b4ed280e06
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 17:48:59 +0100

    hore:  update all spec

--- src/__test__/org/all.spec.ts
+++ src/__test__/org/all.spec.ts
@@ -5,6 +5,7 @@ import { userBuilder } from "../builder";
 it("returns a 401 for unauththenticaged req", async () => {
   await request(app)
     .get(`/api/organisations`)
+    .send()
     .expect(401);
 });
 
@@ -14,6 +15,7 @@ it("returns a 200 for valid request", async () => {
   await request(app)
     .get(`/api/organisations`)
     .set("Cookie", await global.signin())
+    .send()
     .expect(200);
 });
 
