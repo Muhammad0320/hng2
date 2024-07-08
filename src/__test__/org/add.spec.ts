a9e5fe4223ea0c2bb588a643bf35efc33f3c9ce3
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 06:29:29 +0100

    chore:  update add spec test ts

--- src/__test__/org/add.spec.ts
+++ src/__test__/org/add.spec.ts
@@ -1,8 +1,25 @@
 import request from "supertest";
 import app from "../../app";
+import mongoose from "mongoose";
 
 it("returns a 401 for unauthenticated user", async () => {
   await request(app)
     .get("/api/organisations/shitid/users")
     .expect(401);
+});
+
+it("returns a 400 for invalid mongoose id", async () => {
+  await request(app)
+    .get("/api/organisations/shitid/users")
+    .set("Cookie", await global.signin())
+    .expect(400);
+});
+
+it("returns a 404 for valid but umatched userId", async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .get(`/api/organisations/${userId}/users`)
+    .set("Cookie", await global.signin())
+    .expect(404);
 });
