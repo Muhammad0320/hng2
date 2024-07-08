9815134c7d87a712c5e95063bf104e766d6febb9
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 06:21:10 +0100

    chore:  update show test ts

--- src/__test__/auth/show.spec.ts
+++ src/__test__/auth/show.spec.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import app from "../../app";
 import mongoose from "mongoose";
+import { userBuilder } from "../builder";
 
 it("returns a 401 for unauthorized user ", async () => {
   await request(app)
@@ -21,4 +22,13 @@ it("returns a 404 for valid but unmatched id", async () => {
     .get("/api/users/" + new mongoose.Types.ObjectId().toHexString())
     .set("Cookie", await global.signin())
     .expect(404);
+});
+
+it("returns a 200 when everything is valid", async () => {
+  const user = await userBuilder();
+
+  await request(app)
+    .get("/api/users/" + user._id)
+    .set("Cookie", await global.signin())
+    .expect(400);
 });
