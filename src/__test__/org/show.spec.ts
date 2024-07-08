d742f843b97eea4b8f472bd1025005b72ccbfc17
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 06:50:10 +0100

    chore:  update show spec ts

--- src/__test__/org/show.spec.ts
+++ src/__test__/org/show.spec.ts
@@ -7,3 +7,10 @@ it("returns a 401 for unauthorized request", async () => {
 
     .expect(401);
 });
+
+it("returns a 400 for invlalid monogoose id", async () => {
+  await request(app)
+    .get(`/api/organisations/shitid`)
+    .set("Cookie", await global.signin())
+    .expect(400);
+});
