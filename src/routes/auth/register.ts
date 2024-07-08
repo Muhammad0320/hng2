12c06dedf4b7a34b883c3fc3ffa662a255ace928
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 18:06:24 +0100

    chore:  update register ts

--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { BadRequest } from "../../error/BadRequest";
-import User from "../../model/User";
 import { requestValidator } from "../../middleware/requestValidator";
+import User from "../../model/User";
 import {
   emailValidator,
   nameValidator,
@@ -10,7 +10,6 @@ import {
   passwordValidator,
   phoneValidator,
 } from "../../services/validators";
-import Org from "../../model/Organisation";
 
 const router = express.Router();
 
@@ -26,71 +25,53 @@ router.post(
   ],
   requestValidator,
   async (req: Request, res: Response) => {
-                                           const {
-                                             firstName,
-                                             lastName,
-                                             email,
-                                             password,
-                                             phone,
-                                             passwordConfirm,
-                                           } = req.body;
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      phone,
+      passwordConfirm,
+    } = req.body;
 
-                                           const existinguser = await User.findOne(
-                                             { email }
-                                           );
+    const existinguser = await User.findOne({ email });
 
-                                           if (!!existinguser) {
-                                             throw new BadRequest(
-                                               "Registration Unsuccessful"
-                                             );
-                                           }
+    if (!!existinguser) {
+      throw new BadRequest("Registration Unsuccessful");
+    }
 
-                                           const newUSer = await User.buildUser(
-                                             {
-                                               firstName,
-                                               lastName,
-                                               email,
-                                               password,
-                                               passwordConfirm,
-                                               phone,
-                                             }
-                                           );
+    const newUSer = await User.buildUser({
+      firstName,
+      lastName,
+      email,
+      password,
+      passwordConfirm,
+      phone,
+    });
 
-                                           if (!process.env.JWT_EXPIRES_IN)
-                                             throw new Error(
-                                               " Jwt timestamp not found "
-                                             );
+    if (!process.env.JWT_EXPIRES_IN)
+      throw new Error(" Jwt timestamp not found ");
 
-                                           if (!process.env.JWT_SECRET)
-                                             throw new Error(
-                                               " Jwt secret not found "
-                                             );
+    if (!process.env.JWT_SECRET) throw new Error(" Jwt secret not found ");
 
-                                           const accessToken = jwt.sign(
-                                             newUSer,
-                                             process.env.JWT_SECRET,
-                                             {
-                                               expiresIn:
-                                                 +process.env.JWT_EXPIRES_IN *
-                                                 24 *
-                                                 60 *
-                                                 60,
-                                             }
-                                           );
+    const accessToken = jwt.sign(newUSer, process.env.JWT_SECRET, {
+      expiresIn: +process.env.JWT_EXPIRES_IN * 24 * 60 * 60,
+    });
 
-                                           req.session = {
-                                             jwt: accessToken,
-                                           };
+    req.session = {
+      jwt: accessToken,
+    };
 
-                                           res.status(201).json({
-                                             status: "success",
-                                             message: "Registration Successful",
-                                             data: {
-                                               accessToken,
-                                               user: newUSer,
-                                             },
-                                           });
-                                         }
+    res.status(201).json({
+      status: "success",
+      message: "Registration Successful",
+      data: {
+        accessToken,
+        user: newUSer,
+      },
+    });
+  }
 );
 
 export { router as registerUserRouter };
+
