e967e031fd87d13df371c5f39908d86a5e695d19
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 17:54:22 +0100

    chore:  update add ts

--- src/__test__/org/add.spec.ts
+++ src/__test__/org/add.spec.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import app from "../../app";
 import mongoose from "mongoose";
-import { userBuilder } from "../builder";
+import { orgBuilder, userBuilder } from "../builder";
 
 it("returns a 401 for unauthenticated user", async () => {
   await request(app)
@@ -21,24 +21,29 @@ it("returns a 400 for invalid mongoose id", async () => {
 });
 
 it("returns a 404 for valid but umatched userId", async () => {
-  const userId = new mongoose.Types.ObjectId().toHexString();
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  const user = await userBuilder();
 
   await request(app)
-    .get(`/api/organisations/${userId}/users`)
+    .get(`/api/organisations/${id}/users`)
     .set("Cookie", await global.signin())
-    .send()
+    .send({ userId: user.id })
 
     .expect(404);
 });
 
 it("returns a 200 when e/thing is valid", async () => {
-  const { userId } = await userBuilder();
+  const user = await userBuilder();
+
+  const org = await orgBuilder(user.id);
 
   await request(app)
-    .get(`/api/organisations/${userId}/users`)
+    .get(`/api/organisations/${org.id}/users`)
     .set("Cookie", await global.signin())
-    .send()
-    .expect(200);
+    .send({ userId: user.id })
+
+    .expect(404);
 });
 
 
