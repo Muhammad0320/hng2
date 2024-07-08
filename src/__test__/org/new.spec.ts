a809c886e8ff611f5d1c3cbaa6d86312b639dacd
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 17:57:51 +0100

    chore:  update new spec ts

--- src/__test__/org/new.spec.ts
+++ src/__test__/org/new.spec.ts
@@ -1,5 +1,6 @@
 import app from "../../app";
 import request from "supertest";
+import { userBuilder, orgBuilder } from "../builder";
 
 it("returns a 401 for unauth request", async () => {
   await request(app)
@@ -13,5 +14,23 @@ it("returns a 400, for invalid request", async () => {
     .get(`/api/organisations`)
     .set("Cookie", await global.signin())
     .send({ name: "Muhammad org" })
+    .expect(400);
+
+  await request(app)
+    .get(`/api/organisations`)
+    .set("Cookie", await global.signin())
+    .send({ description: "Muhammad org description" })
+    .expect(400);
+});
+
+it("returns a 400 if org name already exists", async () => {
+  const user = await userBuilder();
+
+  const org = await orgBuilder(user.id);
+
+  await request(app)
+    .get(`/api/organisations`)
+    .set("Cookie", await global.signin())
+    .send({ name: org.name })
     .expect(400);
 });
