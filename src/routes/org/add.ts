1e3d97759df43419f765ce46d757060346d202f2
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-08 04:27:00 +0100

    chore:  update add ts

--- src/routes/org/add.ts
+++ src/routes/org/add.ts
@@ -1,5 +1,31 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { requireAuth } from "../../middleware/requireAuth";
+import { paramsChecker } from "../../middleware/paramsChecker";
+import Org from "../../model/Organisation";
+import { NotFound } from "../../error/NotFound";
+import { idValidator } from "../../services/validators";
+import { requestValidator } from "../../middleware/requestValidator";
 
 const router = express.Router();
+
+router.post(
+  "/:id/users",
+  requireAuth,
+  paramsChecker("id"),
+  [idValidator()],
+  requestValidator,
+  async (req: Request, res: Response) => {
+    const org = await Org.findById(req.params.id);
+
+    if (!org) throw new NotFound("Org not found");
+
+    await org.updateOne({ users: [...org.users, req.body.userId] });
+
+    res.status(200).json({
+      status: "success",
+      message: "",
+    });
+  }
+);
 
 export { router as addNewUserToOrg };
