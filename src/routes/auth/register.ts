68586d92cc18f3a34698cfdd9b5904fc4b64a91e
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-09 04:39:55 +0100

    chore:  update register

--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -54,7 +54,9 @@ router.post(
 
     if (!process.env.JWT_SECRET) throw new Error(" Jwt secret not found ");
 
-    const accessToken = jwt.sign(newUSer, process.env.JWT_SECRET, {
+    console.log(newUSer, "from the register------------");
+
+    const accessToken = jwt.sign({ user: newUSer }, process.env.JWT_SECRET, {
       expiresIn: +process.env.JWT_EXPIRES_IN * 24 * 60 * 60,
     });
 
